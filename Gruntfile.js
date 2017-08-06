a42d474cdb2ceeb66e8820152c549d3fc49c736a
Kirill Mukhov <kmukhov@luxoft.com>
2017-08-06 23:48:04 +0300

    simple fix load js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,30 @@
+/**
+ * Load grunt configs
+ * + Recurse concat files
+ * Remove build directory
+ * Render html file by template
+ * build style files
+ * build html by fast template
+ * watching changes in files
+ * load by hotspot
+ * include angular js
+ * build angular templates
+ * build templates angular from each directory
+ * check code by jslint
+ * load test before build files
+ * add mock server for services
+ * add js6 support
+ * go from 1.4 to 1.6 angular
+ * may by add typescript js
+ */
+
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
         concat: {
             js: {
-                src: ['src/js/*.js'],
+                src: ['src/js/**/*.js'],
                 dest: 'build/scripts.js'
             },
             css: {
