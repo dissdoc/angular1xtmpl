8a0e34b2a12885b30803a6d0215a03634776da76
Kirill Mukhov <kmukhov@luxoft.com>
2017-08-07 00:07:02 +0300

    add tasks for release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /**
- * Load grunt configs
+ * + Load grunt configs
  * + Recurse concat files
  * Remove build directory
  * Render html file by template
@@ -13,6 +13,7 @@
  * build templates angular from each directory
  * check code by jslint
  * load test before build files
+ * load reports for test
  * add mock server for services
  * add js6 support
  * go from 1.4 to 1.6 angular
